a8e5f6888036f260e2bf0ba53124c4a7b59cba1d
VR <victor@clarusway.com>
2020-07-04 21:00:04 +0300

    leapyear dosyasına ikinci methodu ekledik. about.txt dosyasını değiştirdik.

--- leapyear.py
+++ leapyear.py
@@ -4,3 +4,11 @@ if (check_year % 4 == 0) and (not (check_year % 100 == 0) or  (check_year % 400 
     print(check_year, " is a leap year.")
 else:
     print(check_year, " is not a leap year!")
+
+
+# # METHOD-2: Shortest solution (wihtout any if-else block or loop)
+
+check_year = int(input("Please input a year for checking if it is leap or not: "))
+
+print((check_year % 4 == 0) and (not (check_year % 100 == 0) or  (check_year % 400 == 0)))
+
