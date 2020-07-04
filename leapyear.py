d7d6b57dc6547e97345a19f330575769a4538ac5
VR <victor@clarusway.com>
2020-07-04 20:57:06 +0300

    leapyear dosyasına method 1 ekledik.

---
+++ leapyear.py
@@ -0,0 +1,6 @@
+check_year = int(input("Please input a year for checking if it is leap or not: "))
+
+if (check_year % 4 == 0) and (not (check_year % 100 == 0) or  (check_year % 400 == 0)):
+    print(check_year, " is a leap year.")
+else:
+    print(check_year, " is not a leap year!")
